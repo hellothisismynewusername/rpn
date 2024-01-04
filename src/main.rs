83a9284d42c9cf18d7852e19bb288f44b867db21
hellothisismynewusername <emahsue2@gmail.com>
2024-01-03 23:51:41 -0800

    simpler code

--- src/main.rs
+++ src/main.rs
@@ -1,142 +1,80 @@
-use std::env;
-
-struct Info {
-    argc : u8,
-}
-
-impl Info {
-    fn default() -> Info {
-        Info {
-            argc: 2,
-        }
-    }
-}
-
 fn main() {
-    let args = env::args();
-    let mut show_steps = false;
-    for arg in args {
-        if arg == "-v" {
-            show_steps = true;
-        }
-    }
-
     let mut input = String::new();
     let mut expression : Vec<String> = Vec::new();
     std::io::stdin().read_line(&mut input).unwrap();
     for i in input.split_whitespace() {
         expression.push(String::from(i));
-    }
-    let mut offset : usize = 0;
-    let mut info : Info = Info::default();
+    };
+    let mut info : Vec<usize> = Vec::new();
+    let mut keywords : Vec<String> = Vec::new();
+    keywords.push(String::from("+"));
+    keywords.push(String::from("-"));
+    keywords.push(String::from("*"));
+    keywords.push(String::from("/"));
+    keywords.push(String::from("sin"));
+    let mut met_at_least_one_keyword = false;
     for i in 0..expression.len() {
-        if show_steps {
-            println!("INDEX IS {}", i - offset);
+        for keyword in &keywords {
+            if &expression[i] == keyword {
+                met_at_least_one_keyword = true;
+            }
         }
-        let before : String = expression[i - offset].clone();
-        if show_steps {
-            println!("BEFORE: {}  |  ", expression[i - offset]);
-        }
-
-        if evaluate(i - offset, &mut expression, &mut info).is_none() {
-            println!("Skipping unparsable part: {}", expression[i - offset]);
-            if !show_steps {
-                println!();
-            }
-            if show_steps {
-                println!("{}", offset);
-            }
-            expression.remove(i - offset);
-            offset += 1;
-
-            if show_steps {
-                print!("OVERALL AFTER DELETION: ");
-                for j in expression.clone() {
-                    print!("{} ", j);
-                }
-            }
-        } else {
-            expression[i - offset] = String::from(evaluate(i - offset, &mut expression, &mut info).unwrap().to_string());
-            let after : String = expression[i - offset].clone();
-            if show_steps {
-                println!("AFTER: {}  |  ", expression[i - offset]);
-            }
-            
-            if show_steps {
-                print!("OVERALL: ");
-                for j in expression.clone() {
-                    print!("{} ", j);
-                }
-            }
-
-            if before.parse::<f64>().is_err() && before != after {
-                if show_steps {
-                    println!("I AM DELETING PREVIOUS {}", info.argc);
-                }
-                for _j in 0..info.argc {
-                    expression.remove(i - offset - info.argc as usize);
-                }
-                offset += info.argc as usize;
-            }
-            
-            /*
-            if before != after {
-                println!("I AM DELETING PREVIOUS {}", info.argc);
-                for _j in 0..info.argc {
-                    expression.remove(i - offset - 2);
-                }
-                offset += info.argc as usize;
-            }
-            */
-
-            if show_steps {
-                print!("OVERALL AFTER DELETION: ");
-                for j in expression.clone() {
-                    print!("{} ", j);
-                }
-            }
-
-            if show_steps {
-                println!();
-            }
+        if expression[i].parse::<f64>().is_err() && !met_at_least_one_keyword {
+            println!("'{}' could not be parsed", expression[i]);
         }
     }
-    println!("{}", expression[expression.len() - 1]);
+    if evaluate(expression.len() - 1, &mut expression, &mut info).is_none() {
+        println!("The expression could not be calculated");
+    } else {
+        println!("{}", evaluate(expression.len() - 1, &mut expression, &mut info).unwrap());
+        /*
+        println!("INFO");
+        for i in &info {
+            print!("{}", i);
+        }
+        println!("LEFTOVER");
+        for i in expression.iter().enumerate() {
+            if !info.contains(&i.0) {
+                print!("{}", i.1);
+            }
+        }
+        */
+    }
 }
 
-fn evaluate(inp : usize, expr : &mut Vec<String>, info : &mut Info) -> Option<f64> {
+fn evaluate(inp : usize, expr : &mut Vec<String>, info : &mut Vec<usize>) -> Option<f64> {
     if expr[inp].parse::<f64>().is_err() {
         //println!("ack!");
         if expr[inp] == "+" {
-            info.argc = 2;
+            info.push(inp);
             if inp <= 1 {
                 return None;
             }
             Some(evaluate(inp - 2, expr, info)? + evaluate(inp - 1, expr, info)?)
 
         } else if expr[inp] == "-" {
-            info.argc = 2;
+            info.push(inp);
             if inp <= 1 {
                 return None;
             }
             Some(evaluate(inp - 2, expr, info)? - evaluate(inp - 1, expr, info)?)
 
         } else if expr[inp] == "*" {
-            info.argc = 2;
+            info.push(inp);
             if inp <= 1 {
                 return None;
             }
             Some(evaluate(inp - 2, expr, info)? * evaluate(inp - 1, expr, info)?)
 
         } else if expr[inp] == "/" {
-            info.argc = 2;
+            info.push(inp);
             if inp <= 1 {
                 return None;
             }
             Some(evaluate(inp - 2, expr, info)? / evaluate(inp - 1, expr, info)?)
 
         } else if expr[inp] == "sin" {
-            info.argc = 1;
+            info.push(inp);
             if inp <= 0 {
                 return None;
             }
