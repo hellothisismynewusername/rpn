2346b8e17250461a13427bc102f9f608f1160062
hellothisismynewusername <emahsue2@gmail.com>
2024-01-04 15:53:13 -0800

    Changes to how it works and more operations

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,33 @@
 fn main() {
+    let mut verbose : bool = false;
+    for arg in std::env::args() {
+        if arg == "-v" {
+            verbose = true;
+        }
+    }
+
     let mut input = String::new();
     let mut expression : Vec<String> = Vec::new();
     std::io::stdin().read_line(&mut input).unwrap();
     for i in input.split_whitespace() {
         expression.push(String::from(i));
     };
-    let mut info : Vec<usize> = Vec::new();
     let mut keywords : Vec<String> = Vec::new();
     keywords.push(String::from("+"));
     keywords.push(String::from("-"));
     keywords.push(String::from("*"));
     keywords.push(String::from("/"));
     keywords.push(String::from("sin"));
+    keywords.push(String::from("cos"));
+    keywords.push(String::from("tan"));
+    keywords.push(String::from("csc"));
+    keywords.push(String::from("sec"));
+    keywords.push(String::from("cot"));
+    keywords.push(String::from("pow"));
+    keywords.push(String::from("root"));
+
+    /*
+    let mut info : Vec<usize> = Vec::new();
     let mut met_at_least_one_keyword = false;
     for i in 0..expression.len() {
         for keyword in &keywords {
@@ -27,54 +43,220 @@ fn main() {
         println!("The expression could not be calculated");
     } else {
         println!("{}", evaluate(expression.len() - 1, &mut expression, &mut info).unwrap());
-        /*
-        println!("INFO");
-        for i in &info {
-            print!("{}", i);
-        }
-        println!("LEFTOVER");
+        
+        println!("LEFTOVER:");
         for i in expression.iter().enumerate() {
             if !info.contains(&i.0) {
                 print!("{}", i.1);
             }
         }
-        */
+        
+    }
+    */
+
+    let mut stack : Vec<String> = Vec::new();
+    let mut counter : usize = 0;
+    while counter < expression.len() {
+        if verbose {
+            println!("Stack on iteration {} BEFORE changes is: ", counter);
+            for i in &stack {
+                print!("{} ", i);
+            }
+        }
+        if verbose { println!(); }
+        stack.push(expression[counter].clone());
+        let stack_length = stack.len();
+        let mut inputs_amount : usize = 0;
+        stack[stack_length - 1] = if is_a_keyword(&stack[stack.len() - 1], &keywords) {
+            if evaluate(stack_length - 1, &stack, &mut inputs_amount, verbose).is_none() {
+                "Err".to_string()
+            } else {
+                evaluate(stack_length - 1, &stack, &mut inputs_amount, verbose).unwrap().to_string()
+            }
+        } else {
+            stack[stack_length - 1].clone()
+        };
+        if verbose { println!("input amount is {}", inputs_amount); }
+        for _i in 0..inputs_amount {
+            stack.remove(stack_length - inputs_amount - 1);
+        };
+        if verbose {
+            println!("Stack on iteration {} AFTER changes is: ", counter);
+            for i in &stack {
+                print!("{} ", i);
+            }
+        }
+        counter += 1;
+    }
+    println!("Output:");
+    for i in &stack {
+        print!("{} ", i);
     }
 }
 
-fn evaluate(inp : usize, expr : &mut Vec<String>, info : &mut Vec<usize>) -> Option<f64> {
+fn is_a_keyword(inp : &String, keywords : &Vec<String>) -> bool {
+    for keyword in keywords {
+        if inp == keyword {
+            return true;
+        }
+    }
+    return false;
+}
+
+fn evaluate(inp : usize, expr : &Vec<String>, inputs_amount : &mut usize, verbose : bool) -> Option<f64> {
+    if verbose { println!("Im at {}, and i see {}", inp, expr[inp]); }
     if expr[inp].parse::<f64>().is_err() {
         //println!("ack!");
         if expr[inp] == "+" {
-            info.push(inp);
+            *inputs_amount = 2;
+            if inp <= 1 {
+                *inputs_amount = 0;
+                None
+            } else {
+                Some(evaluate(inp - 2, expr, inputs_amount, verbose)? + evaluate(inp - 1, expr, inputs_amount, verbose)?)
+            }
+
+        } else if expr[inp] == "-" {
+            *inputs_amount = 2;
+            if inp <= 1 {
+                *inputs_amount = 0;
+                None
+            } else {
+                Some(evaluate(inp - 2, expr, inputs_amount, verbose)? - evaluate(inp - 1, expr, inputs_amount, verbose)?)
+            }
+
+        } else if expr[inp] == "*" {
+            *inputs_amount = 2;
+            if inp <= 1 {
+                *inputs_amount = 0;
+                None
+            } else {
+                Some(evaluate(inp - 2, expr, inputs_amount, verbose)? * evaluate(inp - 1, expr, inputs_amount, verbose)?)
+            }
+
+        } else if expr[inp] == "/" {
+            *inputs_amount = 2;
+            if inp <= 1 {
+                *inputs_amount = 0;
+                None
+            } else {
+                Some(evaluate(inp - 2, expr, inputs_amount, verbose)? / evaluate(inp - 1, expr, inputs_amount, verbose)?)
+            }
+
+        } else if expr[inp] == "pow" {
+            *inputs_amount = 2;
+            if inp <= 1 {
+                *inputs_amount = 0;
+                None
+            } else {
+                Some(evaluate(inp - 2, expr, inputs_amount, verbose)?.powf(evaluate(inp - 1, expr, inputs_amount, verbose)?))
+            }
+
+        } else if expr[inp] == "root" {
+            *inputs_amount = 2;
+            if inp <= 1 {
+                *inputs_amount = 0;
+                None
+            } else {
+                Some(evaluate(inp - 2, expr, inputs_amount, verbose)?.powf(1. / evaluate(inp - 1, expr, inputs_amount, verbose)?))
+            }
+
+        } else if expr[inp] == "sin" {
+            *inputs_amount = 1;
+            if inp <= 0 {
+                *inputs_amount = 0;
+                None
+            } else {
+                Some(evaluate(inp - 1, expr, inputs_amount, verbose)?.sin())
+            }
+
+        } else if expr[inp] == "cos" {
+            *inputs_amount = 1;
+            if inp <= 0 {
+                *inputs_amount = 0;
+                None
+            } else {
+                Some(evaluate(inp - 1, expr, inputs_amount, verbose)?.cos())
+            }
+
+        } else if expr[inp] == "tan" {
+            *inputs_amount = 1;
+            if inp <= 0 {
+                *inputs_amount = 0;
+                None
+            } else {
+                Some(evaluate(inp - 1, expr, inputs_amount, verbose)?.tan())
+            }
+
+        } else if expr[inp] == "csc" {
+            *inputs_amount = 1;
+            if inp <= 0 {
+                *inputs_amount = 0;
+                None
+            } else {
+                Some(1. / (evaluate(inp - 1, expr, inputs_amount, verbose)?.sin()))
+            }
+
+        } else if expr[inp] == "sec" {
+            *inputs_amount = 1;
+            if inp <= 0 {
+                *inputs_amount = 0;
+                None
+            } else {
+                Some(1. / (evaluate(inp - 1, expr, inputs_amount, verbose)?.cos()))
+            }
+
+        } else if expr[inp] == "cot" {
+            *inputs_amount = 1;
+            if inp <= 0 {
+                *inputs_amount = 0;
+                None
+            } else {
+                Some(1. / (evaluate(inp - 1, expr, inputs_amount, verbose)?.tan()))
+            }
+
+        } else {
+            *inputs_amount = 0;
+            None
+        }
+    } else {
+        Some(expr[inp].parse::<f64>().unwrap())
+    }
+}
+
+/*
+fn evaluate(inp : usize, expr : &mut Vec<String>, info : &mut Vec<usize>) -> Option<f64> {
+    println!("Im at {}, and i see {}", inp, expr[inp]);
+    info.push(inp);
+    if expr[inp].parse::<f64>().is_err() {
+        //println!("ack!");
+        if expr[inp] == "+" {
             if inp <= 1 {
                 return None;
             }
             Some(evaluate(inp - 2, expr, info)? + evaluate(inp - 1, expr, info)?)
 
         } else if expr[inp] == "-" {
-            info.push(inp);
             if inp <= 1 {
                 return None;
             }
             Some(evaluate(inp - 2, expr, info)? - evaluate(inp - 1, expr, info)?)
 
         } else if expr[inp] == "*" {
-            info.push(inp);
+            println!("Doing * on {} and {}, which is {} and {}", inp - 2, inp - 1, expr[inp - 2], expr[inp - 1]);
             if inp <= 1 {
                 return None;
             }
             Some(evaluate(inp - 2, expr, info)? * evaluate(inp - 1, expr, info)?)
 
         } else if expr[inp] == "/" {
-            info.push(inp);
             if inp <= 1 {
                 return None;
             }
             Some(evaluate(inp - 2, expr, info)? / evaluate(inp - 1, expr, info)?)
 
         } else if expr[inp] == "sin" {
-            info.push(inp);
+            println!("Doing sin on {}, which is {}", inp - 1, expr[inp - 1]);
             if inp <= 0 {
                 return None;
             }
@@ -96,3 +278,4 @@ fn evaluate(inp : usize, expr : &mut Vec<String>, info : &mut Vec<usize>) -> Opt
         Some(expr[inp].parse::<f64>().unwrap())
     }
 }
+*/
